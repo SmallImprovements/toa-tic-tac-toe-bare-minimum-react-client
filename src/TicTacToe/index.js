bc70c1b8d3f4ca7fcf547b7a2b4ed18c32b5ed07
LFDM <1986gh@gmail.com>
2017-07-10 10:00:08 +0200

    feat(tic-tac-toe) Add bare minimum implementation

--- src/TicTacToe/index.js
+++ src/TicTacToe/index.js
@@ -19,20 +19,95 @@ import './style.css';
 //   }
 // }
 
+const rowStyle = {
+  display: 'flex',
+};
+
+const cellStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'lightcoral',
+  color: 'white',
+  width: '100px',
+  height: '100px',
+  fontSize: '40px',
+  marginRight: '5px',
+  marginBottom: '5px',
+};
+
 class TicTacToe extends React.Component {
   render() {
     const { game, games, isInGame, hasPendingGame, actions, username } = this.props;
+    const { create, join, leave, move } = actions;
 
     return (
       <div>
-        <h1>Hello {username}!</h1>
-
-        <p>
-          <a href="http://35.189.249.30:8080/instructions">Check out the instructions to get started!</a>
-        </p>
+        <div>Hello { username }</div>
+        { isInGame ?
+            <Game username={username} game={game} onMove={move} onLeave={leave} /> :
+            <Lobby games={games} onJoin={join} onCreate={create} />
+        }
       </div>
     );
   }
 }
 
 export default withTicTacToe(TicTacToe);
+
+function Game({ game, username, onMove, onLeave }) {
+  return (
+    <div>
+      <div>
+        { game.status.type === 'FINISHED' ?
+          `${game.status.winner} wins!` :
+          game.turn === username ? 'Make a move' : 'Waiting for other player'
+        }
+      </div>
+
+      <div>
+        { game.board.map((row, y) => {
+          return (
+            <div key={y} style={rowStyle}>
+              {
+                row.map((col, x) => {
+                  let mark = '';
+                  if (game.players[0] === col) { mark = 'X'; }
+                  if (game.players[1] === col) { mark = 'O'; }
+                  const onClick = mark ? () => {} : () => onMove({ y, x });
+                  return (
+                    <div key={x} style={cellStyle} onClick={onClick}>{ mark }</div>
+                  );
+                })
+              }
+            </div>
+          );
+        })}
+      </div>
+      <button onClick={onLeave}>Leave</button>
+    </div>
+  );
+}
+
+function Lobby({ games, onJoin, onCreate }) {
+  return (
+    <div>
+        <div>
+            <button onClick={onCreate}>Create new game</button>
+        </div>
+
+        <h1>Pending Games</h1>
+        <ul>
+            { games.map((game) => {
+              return (
+                <li key={game.gameUid}>
+                  <button onClick={() => onJoin(game.gameUid) }>
+                     { game.players[0] }
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+    </div>
+  );
+}
